bc74e8b1e2b38d39454dccc88fdda8e70863ab54
Mengsen-W <mengsen_wang@163.com>
2020-08-17 08:46:35 +0800

    mutilthread

---
+++ mutilthread/src/main.rs
@@ -0,0 +1,101 @@
+use std::thread;
+use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
+use std::time::Duration;
+
+fn main() {
+    thread_test();
+    channel_test();
+    shared_state_test();
+
+}
+
+fn thread_test() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+
+        }
+
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+
+    }
+
+    handle.join().unwrap();
+
+}
+
+fn channel_test() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+
+        }
+
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+
+        }
+
+    });
+
+    // 阻塞接收知道发送通道关闭
+    for received in rx {
+        println!("Got: {}", received);
+
+    }
+
+}
+
+fn shared_state_test() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+
+        });
+        handles.push(handle);
+
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
