0af0da4ee15956d88bc9779eed17f6c985ecc82e
Mengsen-W <mengsen_wang@163.com>
2020-09-05 22:23:21 +0800

    webserver

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,4 +1,7 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,7 +9,22 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection Established!");
+        handle_connection(stream);
     }
     println!("Hello, world!");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let contents = fs::read_to_string("helloRust").unwrap();
+
+    let response = format!("echo: {}{}", String::from_utf8_lossy(&buffer[..]), contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
