289502cec053a08d25c1d2ef0c51f33bd47d5656
mengsen <mengsen_wang@163.com>
2020-11-11 22:49:21 +0800

    chapter 4

--- Tao_of_Rust/chapter_4/src/main.rs
+++ Tao_of_Rust/chapter_4/src/main.rs
@@ -2,6 +2,18 @@
 /// mengsen
 /// 2020-11-11 17:49:13
 
+// inline and all runtime life
+const MAX_NUM: u32 = 1;
+
+// not inline and all runtime life
+static MIN_NUM: u32 = 1;
+
 fn main() {
-    println!("Hello, world!");
+    {
+        test_const_and_static();
+    }
+}
+
+fn test_const_and_static() {
+    assert_eq!(MAX_NUM, MIN_NUM);
 }
