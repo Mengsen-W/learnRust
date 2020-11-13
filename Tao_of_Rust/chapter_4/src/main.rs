0d780d6426762651712c1d44e5c7461e9d1ce1f5
Mengsen <mengsen_wang@163.com>
2020-11-13 10:18:19 +0800

    chapter 4 finished

--- Tao_of_Rust/chapter_4/src/main.rs
+++ Tao_of_Rust/chapter_4/src/main.rs
@@ -20,6 +20,13 @@ fn main() {
         let array: [i32; return_num(2) as usize] = [5; return_num(2) as usize];
         println!("{:?}", array)
     }
+
+    {
+        mem_leak();
+    }
+    {
+        mem_distribution();
+    }
 }
 
 fn test_const_and_static() {
@@ -28,4 +35,59 @@ fn test_const_and_static() {
 
 const fn return_num(num: i32) -> i32 {
     return num;
+}
+
+fn mem_leak() {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    type NodePtr<T> = Option<Rc<RefCell<Node<T>>>>;
+    #[allow(dead_code)]
+    struct Node<T> {
+        data: T,
+        next: NodePtr<T>,
+    }
+    impl<T> Drop for Node<T> {
+        fn drop(&mut self) {
+            println!("Dropping!");
+        }
+    }
+
+    let first = Rc::new(RefCell::new(Node {
+        data: 1,
+        next: None,
+    }));
+    let second = Rc::new(RefCell::new(Node {
+        data: 2,
+        next: Some(first.clone()),
+    }));
+    // circular reference
+    first.borrow_mut().next = Some(second.clone());
+    second.borrow_mut().next = Some(first.clone());
+
+    // no dropping call
+}
+
+#[allow(dead_code)]
+fn mem_distribution() {
+    struct A {
+        a: u32,
+        b: Box<u64>,
+    }
+    struct B(i32, f64, char);
+    struct N;
+    enum E {
+        H(u32),
+        M(Box<u32>),
+    }
+    union U {
+        u: u32,
+        v: u64,
+    }
+
+    println!("Box<u32>: {:?}", std::mem::size_of::<Box<u32>>());
+    println!("A: {:?}", std::mem::size_of::<A>());
+    println!("B: {:?}", std::mem::size_of::<B>());
+    println!("N: {:?}", std::mem::size_of::<N>());
+    println!("E: {:?}", std::mem::size_of::<E>());
+    println!("U: {:?}", std::mem::size_of::<U>());
 }
