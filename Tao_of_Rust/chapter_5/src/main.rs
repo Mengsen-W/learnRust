527d43e535727334e8d16069c91fe5c6364d513c
Mengsen <mengsen_wang@163.com>
2020-11-19 18:02:13 +0800

    finished chapter 5

--- Tao_of_Rust/chapter_5/src/main.rs
+++ Tao_of_Rust/chapter_5/src/main.rs
@@ -31,6 +31,24 @@ fn main() {
   {
     life_limit();
   }
+  {
+    smart_pointer();
+  }
+  {
+    rc_and_weak();
+  }
+  {
+    cell_and_ref_cell();
+  }
+  {
+    copy_on_write();
+  }
+  {
+    token();
+  }
+  {
+    none_lexical_lifetime();
+  }
 }
 
 #[allow(unused_variables)]
@@ -195,4 +213,161 @@ fn life_limit() {
   fn _foo<'a>(s: &'a [u32]) -> Box<dyn _Foo<'a> + 'a> {
     Box::new(_FooImpl { s: s })
   }
+}
+
+fn smart_pointer() {
+  // copy
+  let a = Box::new("hello");
+  // move
+  let b = Box::new("Rust".to_string());
+  let c = *a;
+  let d = *b;
+  println!("a = {:?}", a);
+  println!("c = {:?}", c);
+  println!("d = {:?}", d);
+  // println!("{:?}", b);
+
+  use std::rc::Rc;
+  use std::sync::Arc;
+  let r = Rc::new("Rust".to_string());
+  let a = Arc::new(vec![1.0, 2.0, 3.0]);
+  // move occurs
+  let x = &*r;
+  println!("{:?}", x);
+  let f = &*a;
+  println!("{:?}", f);
+}
+
+fn rc_and_weak() {
+  use std::rc::Rc;
+  let x = Rc::new(5);
+  let y1 = x.clone(); // add strong count
+  let _y2 = y1.clone(); // add strong count
+  println!("{:?}", Rc::strong_count(&x));
+  let _w = Rc::downgrade(&x); // add weak count
+  println!("{:?}", Rc::weak_count(&x));
+  let _y3 = &*x; // no add count
+  println!("{}", 100 - *x);
+}
+
+fn cell_and_ref_cell() {
+  use std::cell::Cell;
+  struct Foo {
+    x: u32,
+    y: Cell<u32>,
+  }
+
+  let foo = Foo {
+    x: 1,
+    y: Cell::new(3),
+  };
+  assert_eq!(1, foo.x);
+  assert_eq!(3, foo.y.get());
+  foo.y.set(5);
+  assert_eq!(5, foo.y.get());
+
+  use std::cell::RefCell;
+  let x = RefCell::new(vec![1, 2, 3, 4]);
+  println!("{:?}", x.borrow());
+  x.borrow_mut().push(5);
+  println!("{:?}", x.borrow());
+  {
+    let x = RefCell::new(vec![1, 2, 3, 4]);
+    let mut _v = x.borrow_mut();
+    _v.push(5);
+    // panic
+    // let _m = x.borrow_mut();
+    println!("{:?}", _v);
+  }
+}
+
+fn copy_on_write() {
+  use std::borrow::Cow;
+  fn abs_all(input: &mut Cow<[i32]>) {
+    for i in 0..input.len() {
+      // first call first create
+      let v = input[i];
+      if v < 0 {
+        input.to_mut()[i] = -v;
+      }
+    }
+  }
+
+  fn abs_sum(ns: &[i32]) -> i32 {
+    let mut lst = Cow::from(ns);
+    abs_all(&mut lst);
+    lst.iter().fold(0, |acc, &n| acc + n)
+  }
+
+  // no variable demand no clone
+  let s1 = [1, 2, 3];
+  let mut l1 = Cow::from(&s1[..]);
+  abs_all(&mut l1);
+  println!("IN {:?}", s1);
+  println!("OUT {:?}", l1);
+
+  // variable demand clone
+  let s2 = [1, -2, 3];
+  let mut l2 = Cow::from(&s2[..]);
+  abs_all(&mut l2);
+  println!("IN {:?}", s2);
+  println!("OUT {:?}", l2);
+
+  // no clone because v1 mutable
+  let mut v1 = Cow::from(vec![1, -2, 3]);
+  abs_all(&mut v1);
+  println!("IN OUT {:?}", v1);
+
+  // no variable demand no clone
+  let s3 = [1, 3, 5, 6];
+  let suml = abs_sum(&s3[..]);
+  println!("{:?}", s3);
+  println!("{:?}", suml);
+
+  // variable demand clone
+  let s4 = [1, -3, 5, -6];
+  let sum2 = abs_sum(&s4[..]);
+  println!("{:?}", s4);
+  println!("{:?}", sum2);
+}
+
+fn token() {
+  use std::borrow::Cow;
+  use std::thread;
+  #[derive(Debug)]
+  struct Token<'a> {
+    raw: Cow<'a, str>,
+  }
+  impl<'a> Token<'a> {
+    pub fn new<S>(raw: S) -> Token<'a>
+    where
+      S: Into<Cow<'a, str>>,
+    {
+      Token { raw: raw.into() }
+    }
+  }
+
+  let _token = Token::new("abs123");
+  let token = Token::new("api.example.io".to_string());
+  thread::spawn(move || {
+    println!("token: {:?}", token);
+  })
+  .join()
+  .unwrap();
+
+  let raw = String::from("abc");
+  // borrowed value does not live long enough
+  // argument requires that `raw` is borrowed for `'static
+  // let s = &raw[..];
+  let s = raw;
+  let token = Token::new(s);
+  thread::spawn(move || {
+    println!("token: {:?}", token);
+  })
+  .join()
+  .unwrap();
+}
+
+fn none_lexical_lifetime() {
+  println!("I don't understand o(╥﹏╥)o");
 }
