140f2a1ad59182191271fa3c974a2211aa3410e7
Mengsen <mengsen_wang@163.com>
2020-11-23 21:00:42 +0800

    leetcode

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -10,21 +10,35 @@ pub fn find_min_arrow_shots(mut points: Vec<Vec<i32>>) -> i32 {
     for i in points.iter() {
         let left = i[0];
         let right = i[1];
-        let mut flag: &i32 = &i32::MAX;
-        if let Some(value) = num.last() {
-            flag = &value[1]
-        };
-        if num.is_empty() || *flag < left {
+
+        if num.is_empty()
+            || Some(&left)
+                > match num.last() {
+                    Some(last) => last.last(),
+                    None => break,
+                }
+        {
             num.push(vec![left, right]);
         } else {
             if let Some(last) = num.last_mut() {
                 if let Some(val) = last.last_mut() {
-                    let value = cmp::min(*val, right);
-                    *val = value;
+                    *val = cmp::min(*val, right);
                 }
             }
         }
     }
 
     num.len() as i32
+}
+
+fn test() {
+    let num = vec![vec![1, 2], vec![3, 4]];
+    if Some(&3)
+        >= match num.last() {
+            Some(last) => last.last(),
+            None => return,
+        }
+    {
+        println!("hello");
+    }
 }
