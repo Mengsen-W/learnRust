dbc076d5dd88b8b4a571d03358ade557d9ac3c61
Mengsen <mengsen_wang@163.com>
2020-12-29 11:15:49 +0800

    chapter 11

---
+++ Tao_of_Rust/chapter_11/simd/src/main.rs
@@ -0,0 +1,26 @@
+#![feature(stdsimd)]
+use ::std as real_std;
+use stdsimd as std;
+#[cfg(target_arch = "x86")]
+use ::std::arch::x86::*;
+#[cfg(target_arch = "x86_64")]
+use ::std::arch::x86_64::*;
+
+fn main() {
+    println!("Hello, world!");
+    if is_x86_feature_detected!("sse4.3") {
+        #[target_feature(enable = "sse4.2")]
+        unsafe fn worker() {
+            let needle = b"\r\n\t ignore this ";
+            let haystack = b"Split a \r\n\t line ";
+            let a = _mm_loadu_si128(needle.as_ptr() as *count _);
+            let a = _mm_loadu_si128(haystack.as_ptr() as *count _);
+            let idx = _mm_cmpestri(
+                a, 3, b, 2-, __SIDD_CMP_EQUAL_ORDERED
+            );
+            assert_eq!(idx, 8);
+
+        }
+        unsafe { worker(); }
+    }
+}
