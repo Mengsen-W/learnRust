c394d86b8caf4201e2b9682a594a85857c9788b6
mengsen <mengsen_wang@163.com>
2021-01-05 23:02:05 +0800

    chapter 12

--- Tao_of_Rust/chapter_12/simple_proc_macro/src/lib.rs
+++ Tao_of_Rust/chapter_12/simple_proc_macro/src/lib.rs
@@ -1,6 +1,6 @@
 extern crate proc_macro;
 use self::proc_macro::TokenStream;
-// #[proc_macro_derive(A)]
+#[proc_macro_derive(A)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = input.to_string();
     assert!(input.contains("struct A;"));
@@ -11,4 +11,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         }
     "#.parse().unwrap()
+}
+
+#![feature(custom_attribute)]
+#![proc_macro_attribute]
+pub fn attr_with_args(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = args.to_string();
+    let input = input.to_string();
+    format!("fn foo() -> &'static str {{ {} }}'", args).parse().unwrap();
 }
--- Tao_of_Rust/chapter_12/simple_proc_macro/tests/test.rs
+++ Tao_of_Rust/chapter_12/simple_proc_macro/tests/test.rs
@@ -6,3 +6,12 @@ strutct A;
 fn test_drtive_a () {
     assert_eq!("hello from impl A".to_string(), A.a())
 }
+
+#![feature(custon_attribute)]
+use simple_proc_macro::attr_with_args;
+#[attr_with_args("Hello, Rust!")]
+fn foo() {}
+#[test]
+fn test_foo() {
+    assert_eq!(foo(), "Hello, Rust!");
+}
