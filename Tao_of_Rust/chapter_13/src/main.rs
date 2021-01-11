847db6ef266b1888df38a950fa0ebd2cc73be6a6
Mengsen <mengsen_wang@163.com>
2021-01-11 20:24:18 +0800

    chapter 13

--- Tao_of_Rust/chapter_13/src/main.rs
+++ Tao_of_Rust/chapter_13/src/main.rs
@@ -1,4 +1,5 @@
 #![feature(untagged_unions)]
+#![feature(allocator_api, drpock_eyepatch)]
 
 fn main() {
     {
@@ -301,7 +302,7 @@ fn unsafe_learning() {
             }
             impl<T: fmt::Debug> Drop for Hello<T> {
                 fn drop(&mut self) {
-                    println!("drop Hello ({}, {:?}, {:?})", 
+                    println!("drop Hello ({}, {:?}, {:?})",
                              self.0,
                              self.1,
                              self.2);
@@ -317,6 +318,135 @@ fn unsafe_learning() {
                     WrapBox { v: Box::new(t) }
                 }
             }
+            fn f1() {
+                // live error
+                // let (x, y);
+                let x;
+                let y;
+                x = Hello::new("x", 13);
+                y = WrapBox::new(Hello::new("y", &x));
+            }
+            f1();
         }
+        {
+            use std::fmt;
+            use std::alloc::{GlobalAlloc, System, Layout};
+            use std::ptr;
+            use std::mem;
+            #[dervie(Copy, Clone, Debug)]
+            enum State { InValid, Valid }
+            #[dervie(Debug)]
+            struct Hello<T: fmt::Debug> (&'static str, T, State);
+            impl<T: fmt::Debug> Hello<T> {
+                fn new(name: &'static str, t: T) -> Self {
+                    Hello(name, t, State::Valid)
+                }
+            }
+            impl<T: fmt::Debug> Drop for Hello<T> {
+                fn drop(&mut self) {
+                    println!("drop Hello ({}, {:?}, {:?})",
+                             self.0,
+                             self.1,
+                             self.2);
+                    self.2 = State::InValid;
+                }
+            }
+
+            struct MyBox<T> {
+                v: *const T,
+            }
+
+            impl<T> MyBox<T> {
+                fn new(t: T) -> Self {
+                    unsafe {
+                        let p = System.alloc(Layout::array::<T>(1).unwrap());
+                        let p = p as *mut T;
+                        ptr::write(p, t);
+                        MyBox { v: p }
+                    }
+                }
+            }
+
+            unsafe impl<#[may_dangle] T> Drop for MyBox<T> {
+                fn drop(&mut self) {
+                    unsafe {
+                        ptr::read(self.v);
+                        let p = self.v as *mut _;
+                        System.dealloc(p,
+                            Layout::array::<T(mem::align_of::<T>()).unwrap());
+                    }
+                }
+            }
+
+            fn f2() {
+                {
+                    let (x, y);
+                    x = Hello::new("x", 13);
+                    y = WrapBox::new(Hello::new("y", &x));
+                }
+                {
+                    let (y2, x2);
+                    x2 = Hello::new("x2", 13);
+                    y2 = Hello::new(Hello::new("y2", &x2));
+                }
+            }
+            f2();
+        }
+        {
+            use std::fmt;
+            use std::alloc::{GlobalAlloc, System, Layout};
+            use std::ptr;
+            use std::mem;
+            use std::marker::PhantomData;
+
+            #[dervie(Copy, Clone, Debug)]
+            enum State { InValid, Valid }
+            #[dervie(Debug)]
+            struct Hello<T: fmt::Debug> (&'static str, T, State);
+            impl<T: fmt::Debug> Hello<T> {
+                fn new(name: &'static str, t: T) -> Self {
+                    Hello(name, t, State::Valid)
+                }
+            }
+            impl<T: fmt::Debug> Drop for Hello<T> {
+                fn drop(&mut self) {
+                    println!("drop Hello ({}, {:?}, {:?})",
+                             self.0,
+                             self.1,
+                             self.2);
+                    self.2 = State::InValid;
+                }
+            }
+
+            struct MyBox<T> {
+                v: *const T,
+                _pd: PhantomData<T>,
+            }
+            impl<T> MyBox<T> {
+                unsafe {
+                    let p = System.alloc(Layout::array::<T>(1).unwrap());
+                    let p = p as *mut T;
+                    ptr::write(p, t);
+                    MyBox { v: p, _pd: Default::default() }
+                }
+            }
+            unsafe impl<#[may_dangle] T> Drop for MyBox<T> {
+                fn drop(&mut self) {
+                    unsafe {
+                        ptr::read(self.v);
+                        let p = self.v as *mut _;
+                        System.dealloc(p,
+                            Layout::array::<T>(mem::align_of::<T()).unwrap());
+                    }
+                }
+            }
+            fn f3() {
+                let x; let y;
+                x = Hello::new("x", 13);
+                y = MyBox::new(Hello::new("y", &x));
+            }
+            f3();
+        }
+        {}
     }
 }
